57bd908aa97116272147807391a729132e25bb97
Javo <javovelez@gmail.com>
2022-10-14 16:24:34 -0300

    Agrega detección sin que salten errores pero no parece detectar bien

--- src/lib/models/decode.py
+++ src/lib/models/decode.py
@@ -601,3 +601,33 @@ def multi_pose_decode(
   detections = torch.cat([bboxes, scores, kps, clses], dim=2)
     
   return detections
+
+def polygondet_decode(heat, cl, reg=None, cat_spec_wh=False, K=100):
+    batch, cat, height, width = heat.size()
+
+    # heat = torch.sigmoid(heat)
+    # perform nms on heatmaps
+    heat = _nms(heat)
+
+    scores, inds, clses, ys, xs = _topk(heat, K=K)
+    if reg is not None:
+        reg = _tranpose_and_gather_feat(reg, inds)
+        reg = reg.view(batch, K, 2)
+        xs = xs.view(batch, K, 1) + reg[:, :, 0:1]
+        ys = ys.view(batch, K, 1) + reg[:, :, 1:2]
+    else:
+        xs = xs.view(batch, K, 1) + 0.5
+        ys = ys.view(batch, K, 1) + 0.5
+    cl = _tranpose_and_gather_feat(cl, inds)
+
+    cl = cl.view(batch, K, 8)
+    clses = clses.view(batch, K, 1).float()
+    scores = scores.view(batch, K, 1)
+    # print(scores)
+    polygons = torch.cat([xs, ys, cl], dim =2)
+    # bboxes = torch.cat([xs - wh[..., 0:1] / 2,
+    #                     ys - wh[..., 1:2] / 2,
+    #                     xs + wh[..., 0:1] / 2,
+    #                     ys + wh[..., 1:2] / 2], dim=2)
+    detections = torch.cat([polygons, scores, clses], dim=2)
+    return detections
