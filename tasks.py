314fafb093c5e321cec00a3b3bc2f6d9ae8e1e27
franveggiani <francoveggiani@gmail.com>
2025-05-12 11:35:47 -0300

    celery

---
+++ tasks.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from .schemas import DetectorRequest
+from src.lib.opts import opts
+import os
+import torch
+from src.lib.detectors.circledet_iou import CircledetIOUDetector
+from .api.utils import process_videos
+
+from celery import Celery
+
+app = Celery('detectorBayas', broker='redis://redis:6379/0')
+
+SHARED_PATH = '/shared'
+
+@app.task
+def detector(input_folder, output_folder, video_name):
+    
+    # Determinar donde guardar el output
+    
+    torch.backends.cudnn.enabled = False
+
+    Detector = CircledetIOUDetector
+    
+    args = [
+        'cdiou',
+        '-if', str(input_folder),
+        '-of', str(output_folder),
+        '--load_model', '2022.11.30_grapes_mix_iou.pth'
+    ]
+        
+    opt = opts().init(args)
+    
+    os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
+    
+    opt.detector_for_track = Detector(opt)
+    opt.demo = os.path.join(input_folder + video_name)
+    
+    process_videos(opt, video_name)
+    
+    return {"message": "Detección completada", "video_name": video_name, "output_folder": output_folder}
